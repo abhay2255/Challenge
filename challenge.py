6388cbc44f911cdd62087bbc1dbcafb321d481bd
abhay2255 <58613127+abhay2255@users.noreply.github.com>
2022-12-10 16:08:15 +0530

    challenge

---
+++ challenge.py
@@ -0,0 +1,13 @@
+a=int(input('Input:'))
+i=0
+print("Output:\n")
+if a%2==0:
+    for i in range(i,a):
+        print(a)
+        a=a+2
+        i=i+1
+else:
+    for i in range(i,a):
+        print(a)
+        a=a+2
+        i=i+1
